a2adc2c81d5695f24833809001193449d00bc7a8
GoGoMosh <KE16012001@yandex.ru>
2024-11-02 04:28:22 +0300

    Upgrade

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-import
+import os
+import random
+import requests
+from time import perf_counter
 
 def main():
 
-
-
+    link = 'https://thispersondoesnotexist.com/image'
 
 if __name__ == '__main__':
     main()
