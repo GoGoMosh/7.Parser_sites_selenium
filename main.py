36db284d3c936cab140a997a9df931a6bce74426
GoGoMosh <KE16012001@yandex.ru>
2024-11-04 21:16:14 +0300

    Upgrade

--- main.py
+++ main.py
@@ -1,30 +1,31 @@
 import os
 import random
 from turtledemo.penrose import start
-import requests as req
+from selenium import webdriver
 from time import perf_counter
 import time
+from bs4 import BeautifulSoup
 
 def main():
 
-    link = 'https://thispersonnotexist.org/downloadimage/Ac3RhdGljL21hbi9zZWVkMjE4MjEuanBlZw=='
-    proxy = "185.21.60.38:9999"
-    username = 'kqscyp4bkz-corp.mobile.res-country-NL-state-2743698-city-2747891-hold-session-session-6728e00108632:6AwTjJNpSE9n4exb'
-    imag_count = 10
+    link = 'https://thispersonnotexist.org'
 
-    proxies = {
-        'http': f'http://nwlra1yqrz-corp.mobile.res-country-RU-state-524894-city-524901-hold-session-session-6728e5d0a317e:iHPs440r3LbXI5Q9@93.190.142.57:9999',
-        'https': f'http://nwlra1yqrz-corp.mobile.res-country-RU-state-524894-city-524901-hold-session-session-6728e5d0a317e:iHPs440r3LbXI5Q9@93.190.142.57:9999'
-    }
+    ans = req.get(link, proxies=None).text
+    soup = BeautifulSoup(ans, 'lxml')
 
-    for imag_num in range(imag_count):
-        ans = req.get(link, proxies=None)
-        extension = ans.headers['content-type'].split('/')[-1]
+    # находим блок с картинками
+    block = soup.find('div', class_='tabs')
+    all_image = block.find_all('div', attrs={'xtpx': 'R'})
+
+    print(block)
+    for img in all_image:
+        image_link = img.find('a').get('href')
+        extension = image_link.headers['content-type'].split('/')[-1]
         filename = gener_filename(extension)
 
         with open(os.path.join('image', filename), 'wb') as file:
             file.write(ans.content)
-        print(f'images: {imag_num + 1} finished..')
+        print(f'images: {1} finished..')
 
 def gener_filename(file_extension):
     tmp = str(int(time.time()))
