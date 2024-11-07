c41eb5294077e061eb24e2e8ec0cbf69edc0bcae
GoGoMosh <KE16012001@yandex.ru>
2024-11-07 23:12:12 +0300

    Upgrade

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from turtledemo.penrose import start
 from selenium import webdriver
 from time import perf_counter
 import time
-from bs4 import BeautifulSoup
 
 def main():
 
@@ -12,14 +11,6 @@ def main():
     browser = webdriver.Firefox()
     browser.get('https://thispersonnotexist.org')
     time.sleep(15)
-
-
-def gener_filename(file_extension):
-    tmp = str(int(time.time()))
-    for _ in range(5):
-        tmp += chr(random.randint(65, 75))
-    return f'{tmp}.{file_extension}'
-
 
 
 if __name__ == '__main__':
