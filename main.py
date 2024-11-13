4015e3c05b6803ced1e24e358e679a1fc7ef4f19
GoGoMosh <KE16012001@yandex.ru>
2024-11-13 22:55:57 +0300

    Upgrade

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def main():
         browser.close()
         browser.quit()
     """
-    # Т. к. веб-страница загружает картинки динамически, то придётся использовать selenium для обработкииииииииии
+    # Т. к. веб-страница загружает картинки динамически, то придётся использовать selenium для обработкиииииииии
     browser = webdriver.Firefox() # С FireFox легче работает, потому что сильно гибок в настройках
     browser.get('https://thispersonnotexist.org')
     time.sleep(5)
